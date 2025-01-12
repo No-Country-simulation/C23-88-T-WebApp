f2e4bff103073a70e7dd3cd724b93ae1579f1434
Matias Ruiz <matiasruiz94m@gmail.com>
2025-01-11 22:27:36 -0300

    DB integration and Tables Reverse Ingineer

---
+++ Backend/APIBanca/Model/DTO/UsuarioDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DTO
+{
+    public class UsuarioDTO
+    {
+        public int dni { get; set; }
+        public int id_cuenta { get; set; }
+    }
+}
